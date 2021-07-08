7492e0bb95b413d9382dc2963c2feafc88a059ab
pgreze <pierrick.greze@gmail.com>
2021-07-08 14:48:28 +0900

    Restrict domains + add back/forward buttons

--- hacking4/hacking4/ViewController.swift
+++ hacking4/hacking4/ViewController.swift
@@ -14,11 +14,17 @@ class ViewController: UIViewController {
         view.navigationDelegate = self
         return view
     }()
+    lazy var backButton: UIBarButtonItem = .init(title: "Back", style: .plain, target: self, action: #selector(goBack))
+    lazy var forwardButton: UIBarButtonItem = .init(title: "Forward", style: .plain, target: self, action: #selector(goForward))
     lazy var progressView: UIProgressView = {
         let view = UIProgressView(progressViewStyle: .default)
         view.sizeToFit()
         return view
     }()
+    let urls: [String] = [
+        "pgreze.dev",
+        "mercari.com"
+    ]
 
     override func loadView() {
         view = webView
@@ -29,23 +35,26 @@ class ViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        backButton.isEnabled = false
+        forwardButton.isEnabled = false
         // Allows to have the refresh button on the right.
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         let progressButton = UIBarButtonItem(customView: progressView)
-        toolbarItems = [progressButton, spacer, refresh]
+        toolbarItems = [backButton, forwardButton, progressButton, spacer, refresh]
         navigationController?.isToolbarHidden = false
         
         webView.allowsBackForwardNavigationGestures = true
         // Listen for webView.estimatedProgress updates
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
-        openUrl("pgreze.dev")
+        openUrl(urls[0])
     }
     
     @objc private func openTapped() {
         let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "pgreze.dev", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "mercari.com", style: .default, handler: openPage))
+        urls.forEach { (url) in
+            ac.addAction(UIAlertAction(title: url, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -67,11 +76,35 @@ class ViewController: UIViewController {
             progressView.progress = Float(webView.estimatedProgress)
         }
     }
+    
+    @objc private func goBack() {
+        webView.goBack()
+    }
+    
+    @objc private func goForward() {
+        webView.goForward()
+    }
 }
 
 extension ViewController : WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.url?.host
         progressView.isHidden = true
+        
+        backButton.isEnabled = !webView.backForwardList.backList.isEmpty
+        forwardButton.isEnabled = !webView.backForwardList.forwardList.isEmpty
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        guard let host = navigationAction.request.url?.host else { return }
+        if !urls.filter({ (url) -> Bool in host.hasSuffix(url) }).isEmpty {
+            decisionHandler(.allow)
+        } else {
+            decisionHandler(.cancel)
+
+            let ac = UIAlertController(title: "Forbidden", message: "Cannot access the external domain \(host)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(ac, animated: true)
+        }
     }
 }
