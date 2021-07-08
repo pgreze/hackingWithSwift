a3ea6984dd43b7c0956cf27091f930313cd62db8
pgreze <pierrick.greze@gmail.com>
2021-07-08 11:11:48 +0900

    Add a WKWebView as the root view

--- hacking4/hacking4/ViewController.swift
+++ hacking4/hacking4/ViewController.swift
@@ -6,11 +6,27 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    lazy var webView: WKWebView = {
+        let view = WKWebView()
+        view.navigationDelegate = self
+        return view
+    }()
+
+    override func loadView() {
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: "https://pgreze.dev")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 }
 
+extension ViewController : WKNavigationDelegate {
+    
+}
