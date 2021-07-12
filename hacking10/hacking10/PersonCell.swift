e338c959e9b1f3003e926990383a33a8024826a4
pgreze <pierrick.greze@gmail.com>
2021-07-12 12:44:44 +0900

    Setup collectionview

---
+++ hacking10/hacking10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  hacking10
+//
+//  Created by pgreze on 2021/07/12.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
